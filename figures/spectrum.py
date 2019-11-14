917cae6099fd3710ed325de47a2a2b270f237514
Michaël Defferrard <michael.defferrard@epfl.ch>
2019-11-14 16:27:29 +0100

    add climate to spectrum figure

--- figures/spectrum.py
+++ figures/spectrum.py
@@ -5,10 +5,13 @@ import pickle
 
 import numpy as np
 from matplotlib import pyplot as plt
+import h5py
+import pyshtools
 
-#plt.rc('font', family='Latin Modern Roman')
-#plt.rc('text', usetex=True)
-#plt.rc('text.latex', preamble=r'\usepackage{lmodern}')
+# plt.rc('font', family='Latin Modern Roman')  # Latin Modern for text
+# plt.rc('mathtext', fontset='cm')  # Computer Modern for math (default is dejavusans)
+# plt.rc('text', usetex=True)
+# plt.rc('text.latex', preamble=r'\usepackage{lmodern}')
 
 def plot_with_std(x, y, color=None, alpha=0.2, ax=None, **kwargs):
     ystd = np.std(y, axis=0)
@@ -20,41 +23,57 @@ def plot_with_std(x, y, color=None, alpha=0.2, ax=None, **kwargs):
     ax.fill_between(x, ymean - ystd, ymean + ystd, alpha=alpha, color=color)
     return ax
 
+def compute_spectrum_climate():
+    # Data is from year, month, day, hour, run = 2106, 1, 1, 0, 1
+    h5f = h5py.File('example_climate_data.h5', mode='r')
+    data = h5f['climate']['data']  # (16,768,1152) numpy array
+    spectrum = list()
+    for d in data:
+        d = d[::2, ::3]  # Make it square (needed for pyshtools).
+        d = (d - d.min()) / (d.max() - d.min()) * 2 - 1  # Standardize.
+        grid = pyshtools.SHGrid.from_array(d, grid='DH')
+        clm = grid.expand(normalization='4pi')
+        spectrum.append(clm.spectrum())
+    return spectrum
+
 # 1000 objects out of 30k were randomly sampled to be analyzed.
 # The mean was subtracted from each signal in a preprocessing step.
 # The spectrum was computed by healpy.anafast up to lmax=nside (since there's nothing beyond).
 # cl is a dictionary with keys = {0, 1, 2}. cl[feature] is a matrix of shape [1000, 33] where each row is the spectrum of one signal.
-spectrum_shrec17 = pickle.load(open('../figures/spectrum_shrec17.pickle', 'rb'))
+spectrum_shrec17 = pickle.load(open('spectrum_shrec17.pickle', 'rb'))
 spectrum_shrec17 = np.vstack([
     spectrum_shrec17[0],
     spectrum_shrec17[1],
     spectrum_shrec17[2],
 ])
 
-spectrum_cosmo = np.load('../figures/spectrum_cosmo.npz')
+spectrum_cosmo = np.load('spectrum_cosmo.npz')
 spectrum_cosmo = np.vstack([
     spectrum_cosmo['psd_class1'],
     spectrum_cosmo['psd_class2'],
 ])
 
-print("SHREC'17: {} maps with degree at most {}".format(*spectrum_shrec17.shape))
-print("cosmo: {} maps with degree at most {}".format(*spectrum_cosmo.shape))
+# One spectrum per variable (e.g., TMQ, U850, V850, etc.).
+# There doesn't seem to be much variation across dates or runs.
+spectrum_climate = compute_spectrum_climate()
+spectrum_climate = np.vstack(spectrum_climate)
 
 fig, ax = plt.subplots(figsize=(5, 2.5))
 
-l = np.arange(spectrum_shrec17.shape[1])
-y = np.mean(spectrum_shrec17, axis=0) # *l*(l+1)
-scale = np.mean(y[:33])  # or y[1]
-ax.plot(l, y / scale, label="SHREC'17 (depth and normal maps)")
-#plot_with_std(l, spectrum_shrec17/scale, ax=ax, label="SHREC'17 (depth and normal maps)")
+spectrums = [
+    (spectrum_shrec17, "SHREC'17 (depth and normal)"),
+    (spectrum_cosmo, 'cosmology (convergence map)'),
+    (spectrum_climate, 'climate (16 variables)'),
+]
+for spectrum, label in spectrums:
+    print("{}: {} maps with degree at most {}".format(label, *spectrum.shape))
+    #l = np.arange(spectrum.shape[1])
+    y = np.mean(spectrum, axis=0) # *l*(l+1)
+    scale = np.mean(y[:33])  # or y[1]
+    ax.plot(y / scale, label=label)
+    #plot_with_std(l, spectrum/scale, ax=ax, label=label)
 
-l = np.arange(spectrum_cosmo.shape[1])
-y = np.mean(spectrum_cosmo, axis=0) # *l*(l+1)
-scale = np.mean(y[:33])  # or y[1]
-ax.plot(l, y / scale, label='cosmology (convergence map)')
-#plot_with_std(l, spectrum_cosmo/scale, ax=ax, label="cosmology (convergence map)")
-
-ax.legend()
+ax.legend(loc='upper right')
 ax.set_xlim([1, 1e3])
 ax.set_ylim([1e-3, 5e1])
 ax.set_yscale('log')
