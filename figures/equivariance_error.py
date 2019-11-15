96c278bf9dbe5a22e310c965fd597e28d0217569
Michaël Defferrard <michael.defferrard@epfl.ch>
2019-11-15 17:12:07 +0100

    equivariance error figure: simplify code

--- figures/equivariance_error.py
+++ figures/equivariance_error.py
@@ -16,105 +16,85 @@ font = {'family': 'serif',
         'size': 20,
         }
 plt.rcParams.update({'font.size': 18})
+
 # -------------- khasanova-Frossard ------------------
+
 equiv_error_KF = pickle.load(open('equivariance_error/khasanova_frossard.pickle', 'rb'))
+
+markers = ['v', 'x', 'o']
+
 degree_step = {
     32: 3,
     64: 6,
     128: 12,
-    256: 25}
+    256: 25,
+}
 
-bw = 64
-lmax = bw
-degrees = np.arange(0, lmax, degree_step[bw])
-degrees = degrees[1:]
-l1, = ax.loglog(degrees, equiv_error_KF[bw][1:], 'mv-')
-
-bw = 128
-lmax = bw
-degrees = np.arange(0, lmax, degree_step[bw])
-degrees = degrees[1:]
-ax.loglog(degrees, equiv_error_KF[bw][1:], 'mx-', )
-
-bw = 256
-lmax = bw
-degrees = np.arange(0, lmax, degree_step[bw])
-degrees = degrees[1:]
-ax.loglog(degrees, equiv_error_KF[bw][1:], 'mo-', )
-
+for bw, marker in zip([64, 128, 256], markers):
+    lmax = bw
+    degrees = np.arange(0, lmax, degree_step[bw])
+    degrees = degrees[1:]
+    ax.loglog(degrees, equiv_error_KF[bw][1:], f'm{marker}-')
 
 # ----------------- healpix ----------------------
-nsides = [32, 64, 128]
-
-degree_step = {
-            32: 10,
-            64: 10,
-            128: 50,
-            256: 50,
-            512: 100,
-            1024: 200,
-}
 
 equiv_error_V1 = pickle.load(open('equivariance_error/V1 - ||Lf||.pickle', 'rb'))
 equiv_error_8 = pickle.load(open('equivariance_error/V2 - 8 neighbors - ||Lf||.pickle', 'rb'))
 equiv_error_20 = pickle.load(open('equivariance_error/V2 - 20 neighbors - ||Lf||.pickle', 'rb'))
 equiv_error_40 = pickle.load(open('equivariance_error/V2 - 40 neighbors - ||Lf||.pickle', 'rb'))
 
-nside = 32
-lmax = 3*nside-1
-degrees = np.arange(0, lmax+1, degree_step[nside])
-degrees = degrees[1:]
-l2, = ax.loglog(degrees, equiv_error_V1[nside][1:], 'gv-', label='V1')
-l3, = ax.loglog(degrees, equiv_error_8[nside][1:], 'cv-', label='8 neighbors')
-l4, = ax.loglog(degrees, equiv_error_20[nside][1:], 'rv-', label='20 neighbors')
-l5, = ax.loglog(degrees, equiv_error_40[nside][1:], 'bv-', label='40 neighbors')
+nsides = [32, 64, 128]
 
-nside = 64
-lmax = 3*nside-1
-degrees = np.arange(0, lmax+1, degree_step[nside])
-degrees = degrees[1:]
-ax.loglog(degrees, equiv_error_V1[nside][1:], 'gx-', )
-ax.loglog(degrees, equiv_error_8[nside][1:], 'cx-', )
-ax.loglog(degrees, equiv_error_20[nside][1:], 'rx-', )
-ax.loglog(degrees, equiv_error_40[nside][1:], 'bx-', )
+degree_step = {
+    32: 10,
+    64: 10,
+    128: 50,
+    256: 50,
+    512: 100,
+    1024: 200,
+}
 
-nside = 128
-lmax = 3*nside-1
-degrees = np.arange(0, lmax+1, degree_step[nside])
-degrees = degrees[1:]
-ax.loglog(degrees, equiv_error_V1[nside][1:], 'go-')
-ax.loglog(degrees, equiv_error_8[nside][1:], 'co-', )
-ax.loglog(degrees, equiv_error_20[nside][1:], 'ro-', )
-ax.loglog(degrees, equiv_error_40[nside][1:], 'bo-', )
+for nside, marker in zip(nsides, markers):
+    lmax = 3 * nside - 1
+    degrees = np.arange(0, lmax+1, degree_step[nside])
+    degrees = degrees[1:]
+    ax.loglog(degrees, equiv_error_V1[nside][1:], f'g{marker}-')
+    ax.loglog(degrees, equiv_error_8[nside][1:], f'c{marker}-')
+    ax.loglog(degrees, equiv_error_20[nside][1:], f'r{marker}-')
+    ax.loglog(degrees, equiv_error_40[nside][1:], f'b{marker}-')
 
 # -------------- final parameters ------------------
 
 ax.set_xlabel(r'spherical harmonic degree $\ell$', fontdict=font,)
 #ax.set_ylabel(r'mean equivariance error $\overline{E}_{\mathbf{L}, C}$', fontdict=font, )
 ax.set_ylabel(r'mean equivariance error $\overline{E}_{L, C}$', fontdict=font, )
+
 ax.tick_params(axis='both', which='major')
 ax.grid()
-ax.set_xlim([10,np.max(degrees)])
-# ax.legend(prop={'size': 20})
-legend_elements_1 = [Line2D([0], [0], color='m', marker='', markersize=8),
-                     Line2D([0], [0], color='g', marker='', markersize=8),
-                     Line2D([0], [0], color='c', marker='', markersize=8),
-                   Line2D([0], [0], color='r', marker='', markersize=8),
-                   Line2D([0], [0], color='b', marker='', markersize=8)]
-# Add first legend:  only labeled data is included
-leg1 = ['Khasanova & Frossard, $k=4$', 'Perraudin et al., $k=8$']
-leg1.extend([f'$k$-NN graph, $k={k}$ neighbors' for k in [8, 20, 40]])
-leg1 = ax.legend(legend_elements_1, leg1, loc='lower left')
-# Add second legend for the maxes and mins.
-# leg1 will be removed from figure
-legend_elements_2 = [Line2D([0], [0], color='k', marker='v', markersize=8),
-                   Line2D([0], [0], color='k', marker='x', markersize=8),
-                   Line2D([0], [0], color='k', marker='o', markersize=8)]
+ax.set_xlim([10, np.max(degrees)])
 
-#leg2 = ax.legend(legend_elements_2, ['$s=32$', '$s=64$', '$s=128$'],loc='upper right')
-leg2 = ax.legend(legend_elements_2, [rf'$n \propto {s}^2$' for s in [32, 64, 128]], loc='upper right')
-# Manually add the first legend back
-ax.add_artist(leg1)
+# Add first legend: only labeled data is included
+# ax.legend(prop={'size': 20})
+handles = [  # Some fake handles.
+    Line2D([0], [0], color='m', marker='', markersize=8),
+    Line2D([0], [0], color='g', marker='', markersize=8),
+    Line2D([0], [0], color='c', marker='', markersize=8),
+    Line2D([0], [0], color='r', marker='', markersize=8),
+    Line2D([0], [0], color='b', marker='', markersize=8),
+]
+labels = ['Khasanova & Frossard, $k=4$', 'Perraudin et al., $k=8$']
+labels.extend([f'$k$-NN graph, $k={k}$ neighbors' for k in [8, 20, 40]])
+legend = ax.legend(handles, labels, loc='lower left')
+ax.add_artist(legend)  # Manually add the first legend back
+
+# Add second legend for the maxes and mins.
+handles = [  # Some fake handles.
+    Line2D([0], [0], color='k', marker='v', markersize=8),
+    Line2D([0], [0], color='k', marker='x', markersize=8),
+    Line2D([0], [0], color='k', marker='o', markersize=8),
+]
+labels = [rf'$n \propto {s}^2$' for s in [32, 64, 128]]
+ax.legend(handles, labels, loc='upper right')
 
 fig.tight_layout()
 filename = path.splitext(path.basename(__file__))[0] + '.pdf'
